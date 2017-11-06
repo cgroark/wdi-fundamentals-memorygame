27ea11f1c659d241e15d05bbc8871f7c2b9d72ce
Colin Groark <colingroark@Colins-MacBook-Pro.local>
2017-11-05 16:12:06 -0800

    created game logic and udpated directory

---
+++ js/main.js
@@ -0,0 +1,19 @@
+console.log("Up and running!");
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+
+if(cardsInPlay[0] === cardsInPlay[1]){
+  alert("you found a match");
+}else{
+  alert("sorry try again");
+  }
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
+
